b9a311beb3b9c3393e981c84f8c66da11ff3d0dc
Damian <hitoirl1@gmail.com>
2023-01-26 00:18:38 +0100

    mpsc

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,12 +1,23 @@
-use std::{net::TcpStream, io::{Write, Read, BufReader, BufRead}};
+use std::{net::TcpStream, io::Write, thread, sync::mpsc};
 
 use shared::Buffers;
 
 fn main() {
-    let stream = TcpStream::connect(shared::ADDRESS).unwrap();
+    let (tx, rx) = mpsc::channel::<String>();
 
-    let mut buffers = Buffers::new(stream.try_clone().unwrap());
-    buffers.send_message("hello world from client");
-    let response = buffers.read_message();
-    println!("Received response: {}", response);
+    thread::spawn(move || {
+        for received in rx {
+            let stream = TcpStream::connect(shared::ADDRESS).unwrap();
+            let mut buffers = Buffers::new(stream.try_clone().unwrap());
+            buffers.send_message(&received);
+        }
+    });
+
+    loop {
+        print!("Enter a message: ");
+        std::io::stdout().flush().unwrap();
+        let mut line = String::new();
+        std::io::stdin().read_line(&mut line).unwrap();
+        tx.send(line).unwrap();
+    }
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,20 +1,20 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}, thread};
 
 use shared::Buffers;
 
-fn handle_client(stream: TcpStream) {
+fn handle_client(stream: TcpStream) { // todo: keep client connected
     println!("New client: {}", stream.peer_addr().unwrap());
     let mut buffers = Buffers::new(stream);
     let message = buffers.read_message();
     println!("Received message: {}", message);
-    buffers.send_message(&message);
+    //buffers.send_message(&message);
 }
 
 fn main() {
     match TcpListener::bind(shared::ADDRESS) {
         Ok(listener) => {
             for stream in listener.incoming() {
-                handle_client(stream.unwrap());
+                handle_client(stream.unwrap())
             }
         },
         Err(e) => println!("Failed to bind: {}", e)
